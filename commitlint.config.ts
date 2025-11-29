9e9ed8d93df3076ffed2afbbbbb782e83d863be1
Vedant Chainani <vedantchainani1084@gmail.com>
2025-11-29 08:35:37 +0530

    chore: remove unused packages

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -8,10 +8,6 @@ export default {
       [
         // Dependency-related changes
         "deps",
-        // Websites-related changes
-        "web",
-        // UI related changes
-        "ui",
       ],
     ],
     "type-enum": [
